c88469a878fb4fdaa75b856a77eafe33712f05ba
Sanghyuk Park <dropthegit@gmail.com>
2024-01-26 10:50:59 +0900

    스프링, 스벨트 앱프로젝트

---
+++ src/main/java/com/ll/springsvelteapp/SpringSvelteAppApplication.java
@@ -0,0 +1,13 @@
+package com.ll.springsvelteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSvelteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSvelteAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/springsvelteapp/SpringSvelteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springsvelteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSvelteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
